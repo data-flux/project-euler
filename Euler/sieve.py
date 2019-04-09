790d14b687dfaa13486fb3ebd957b9037fc082aa
Dirk Meijer <meijerdwj@liacs.leidenuniv.nl>
2019-04-09 16:11:20 +0200

    problem #4

--- Euler/sieve.py
+++ Euler/sieve.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3.6
+
 class Sieve:
     def __init__(self,limit=1000):
         self.primes = []
@@ -5,6 +7,8 @@ class Sieve:
         self.extend(limit)
 
     def extend(self,limit):
+        if limit <= self.max_seen:
+            raise ValueError("New Limit must be larger than existing limit!")
         cndd = list(range(self.max_seen+1,limit+1))
         for p in self.primes:
             start = cndd[0] - (cndd[0] % p) + p
@@ -13,5 +17,28 @@ class Sieve:
                     cndd.remove(c)
                 except:
                     continue
+        while len(cndd)>0:
+            p = cndd[0]
+            cndd = cndd[1:]
+            self.primes.append(p)
+            if len(cndd)==0:
+                break
+            start = cndd[0] - (cndd[0] % p) + p
+            for c in range(start,limit+1,p):
+                try:
+                    cndd.remove(c)
+                except:
+                    continue
         self.primes += cndd
         self.max_seen = limit
+
+if __name__=="__main__":
+    from tictoc import *
+    tic()
+    Sieve( 10_000)
+    Sieve(100_000)
+    toc()
+    tic()
+    s = Sieve(10_000)
+    s.extend(100_000)
+    toc()
---
+++ projecteuler004.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3.6
+"""
+PROBLEM: 004
+AUTHOR:  Dirk Meijer
+STATUS:  done
+EXPLANATION:
+    The largest palindromic number will have 6 digits (cannot have 7 if > 1000*1000) so we generate 3 digit numbers, starting at 999, and find a palindromic one.
+"""
+
+from Euler.tictoc import *
+from math import log10,ceil
+
+def ispalindromic(n):
+    d = ceil(log10(n))
+    digits = d*[None,]
+    for b in range(d):
+        digits[b] = n%10
+        n //= 10
+
+    return digits == list(reversed(digits))
+
+
+if __name__=="__main__":
+    tic()
+    m = 0
+    for a in range(999,900,-1):
+        for b in range(999,a,-1):
+            if ispalindromic(a*b):
+                m = max(m,a*b)
+    print(m)
+    toc()
