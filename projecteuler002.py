75a372d763253ca40b7982e1737016e7c489dbaf
Dirk Meijer <meijerdwj@liacs.leidenuniv.nl>
2019-04-09 12:20:08 +0200

    solved #2

---
+++ projecteuler002.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3.6
+"""
+PROBLEM: 002
+AUTHOR:  Dirk Meijer
+STATUS:  experimentation
+EXPLANATION:
+    Tracking the sum while counting.
+"""
+
+from Euler.tictoc import *
+
+def fibonacci_even(upper_limit=4e6):
+    a = 1
+    b = 2
+    S = 2
+    while True:
+        a,b = b,a+b
+        if b > upper_limit:
+            return S
+        if b % 2 == 0:
+            S+=b
+    
+
+if __name__=="__main__":
+    tic()
+    print(fibonacci_even())
+    toc()
+    exit()
