9aa4ff700fecb5606176971a60b668807a82344c
Dirk Meijer <meijerdwj@liacs.leidenuniv.nl>
2019-04-16 15:53:12 +0200

    #33 optimized

--- projecteuler033.py
+++ projecteuler033.py
@@ -4,15 +4,13 @@ PROBLEM: 033
 AUTHOR:  Dirk Meijer
 STATUS:  done
 EXPLANATION:
-    answers will be of the form ab/bd=a/d or ba/db=a/d
+    answers will be of the form ab/bd=a/d or ba/db=a/d, work these out for the equations on lines 21 and 25
 """
 
 from Euler.tictoc import *
-from sympy import lcm,S
 from functools import reduce
-from operator import mul
+from fractions  import Fraction
 
-prod = lambda l: reduce(mul,l,1)
 
 if __name__=="__main__":
     tic()
@@ -22,12 +20,13 @@ if __name__=="__main__":
             for a in range(1,d):
                 if 9*a*d + (-10*a + d) * b  == 0:
                     print(("%d%d/%d%d"%(a,b,b,d)))
-                    fractions.append(S("%d%d/%d%d"%(a,b,b,d)))
+                    fractions.append((a,d))
 
                 if 9*a*d + (10*b-a)*b == 0:
                     print(("%d%d/%d%d"%(b,a,d,b)))
-                    fractions.append(S("%d%d/%d%d"%(b,a,d,b)))
+                    fractions.append((a,d))
     
-    print("Product:\n",prod(fractions))
+    num,denom = reduce(lambda f1,f2:(f1[0]*f2[0],f1[1]*f2[1]),fractions,(1,1))
+    print(Fraction(num,denom).denominator)
     toc()
     exit()
