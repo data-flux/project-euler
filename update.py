9f6c21967799d7e1baeb2768b08c7e857e2e325d
Dirk Meijer <meijerdwj@liacs.leidenuniv.nl>
2019-05-06 15:37:28 +0200

    "update script now does git"

--- update.py
+++ update.py
@@ -5,14 +5,13 @@ import re
 import urllib.request
 from html import unescape
 
-LATEST_PROBLEM=668
 
 print("# Project Euler\n")
 print(f"|Problem|{'Title':60}|{'Status':15}|")
 print(f"|------:|{'-'*60}|{'-'*15}|")
 
 files = [f for f in sorted(listdir('.')) if "projecteuler" in f and "000" not in f]
-for i in range(1,LATEST_PROBLEM+1):
+for i in range(1,1000000):
     status = None
     fn = ""
     for f in [f for f in files if f"{i:03d}" in f]:
@@ -28,7 +27,17 @@ for i in range(1,LATEST_PROBLEM+1):
 
     http = str(urllib.request.urlopen(f"https://projecteuler.net/problem={i}").read())
     title = unescape(re.search(r'<h2>(.+)</h2>',http)[1])
+    if title == "Problems Archives":
+        break
     if status == "not-attempted":
         print(f"|{i}|[{title}](https://projecteuler.net/problem={i})|{status}|")
     else:
         print(f"|{i}|[{title}](https://projecteuler.net/problem={i})|[{status}](./{fn})|")
+
+import sys
+from subprocess import call
+if len(sys.argv)>0:
+    message = " ".join(sys.argv[1:])
+    call(["/usr/bin/env", "git", "add", "."],stdout=sys.stderr)
+    call(["/usr/bin/env", "git", "commit", "-a", "-m", f'"{message}"'],stdout=sys.stderr)
+    call(["/usr/bin/env", "git", "push"],stdout=sys.stderr)
